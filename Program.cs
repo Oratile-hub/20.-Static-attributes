b767e190fa33757a476e070c1d9898fcdf4b94d3
Nomthandazo Ndweni <ndweni2658@gmail.com>
2023-09-18 11:02:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.Static_attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Songs song1 = new Songs("Holiday", "Green Day", 115);
+            Songs song2 = new Songs("Billy Jean", "MJ", 200);
+            Console.WriteLine(song1.title);
+            Console.WriteLine(Songs.songCount);
+
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
